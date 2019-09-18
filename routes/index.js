7fba1160342a8aadc45477d08289f44185124131
chimera-code <albar.sabil28@gmail.com>
2019-09-18 18:34:00 +0700

    first commit

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('hbdwidi')
+});
+
+router.get('/text-animation', function(req, res, next) {
+  res.render('text-animation')
+});
+
+module.exports = router;
