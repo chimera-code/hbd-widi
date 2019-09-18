9c0b40846d49f3dadc9008202649641f76a3278b
chimera-code <albar.sabil28@gmail.com>
2019-09-18 18:51:38 +0700

    change named

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('hbdwidi')
+  res.render('hbdwini')
 });
 
 router.get('/text-animation', function(req, res, next) {
